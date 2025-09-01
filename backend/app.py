bb0f95499dd3dbe6c8b7334466bb42163bdf2e75
jhz6353 <22376353@buaa.edu.cn>
2025-09-01 11:13:21 +0800

    new

--- backend/app.py
+++ backend/app.py
@@ -90,5 +90,5 @@ if __name__ == '__main__':
 
     app.run(debug=debug, host='0.0.0.0', port=port)
 
-    app.run(debug=debug, host='0.0.0.0', port=port)
+    # app.run(debug=debug, host='0.0.0.0', port=port)
 
